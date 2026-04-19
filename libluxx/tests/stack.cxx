d3d4d7d7113450e0a1745799924e3446a298515d
lyrahgames <lyrahgames@mailbox.org>
2026-04-19 12:23:29 +0200

    Tweak stack implementation

--- libluxx/tests/stack.cxx
+++ libluxx/tests/stack.cxx
@@ -1,80 +1,71 @@
-#include <lua.hpp>
 #undef NDEBUG
 #include <cassert>
 import std;
 import xstd;
 import luxx;
 
+static_assert(std::random_access_iterator<luxx::stack_iterator>);
+static_assert(std::ranges::random_access_range<luxx::stack_range>);
+static_assert(std::ranges::random_access_range<luxx::stack>);
+
 void print(luxx::stack stack) {
-  for (auto slot : stack) {
-    visit(slot, xstd::match{
-                    [&](auto value) {
-                      std::println("{:>3}: {:<10}", slot.stack_index(),
-                                   slot.type_name());
-                    },
-                    [&](xstd::one_of<luxx::number, luxx::string> auto value) {
-                      std::println("{:>3}: {:<10} = {}", slot.stack_index(),
-                                   slot.type_name(), value);
-                    },
-                    [&](luxx::integer value) {
-                      std::println("{:>3}: {:<10} = {}", slot.stack_index(),
-                                   "integer", value);
-                    },
-                });
-  }
+  std::println("╭─────┬───────");
+  for (auto value : stack | std::views::reverse)
+    std::println("│ {:>3} │ {}", value.stack_index(), value);
+  std::println("╞═════╪═══════");
+  std::println("│ pos │ value");
+  std::println("╰─────┴───────");
 }
 
 int main() {
   luxx::state lua{};
-
-  // luxx::stack_view stack{lua};
-  // stack.stack_dump();
-
-  // stack.push();
-  // stack.push(true);
-  // stack.push(3.14);
-  // stack.push(-123);
-  // stack.push("Hello, World!");
-  // {
-  //   std::string str{"Dynamic String"};
-  //   stack.push(str);
-  // }
-
-  // stack.reserve(10);
-  // for (int i = 0; i < 10; ++i) stack.push(i);
-
-  // stack.rotate(6, -1);
-
-  // luxx::stack_value_view value{stack.native_handle(), -1};
-  // assert(not value.is<bool>());
-  // assert(not value.is<float>());
-  // assert(value.is<std::string_view>());
-
-  // if (auto v = value.as<std::string_view>())
-  //   std::println("stack value = string({})", v.value());
-
-  // lua_setglobal(lua, "test");
-  // lua_getglobal(lua, "test2");
-  // lua_getglobal(lua, "test");
-
-  // stack[5] = stack[1];
-  // auto n   = static_cast<int>(stack[4]);
-
   luxx::stack stack{lua};
+  stack.reserve(10);
 
   print(stack);
+  assert(stack.empty());
+  assert(stack.size() == 0);
 
   stack.push(luxx::nil);
   stack.push();
-  stack.push(-123, 3.14);
+  stack.push(-123, 3.14, 1.0);
   stack.push("a string");
+  stack.push(true, false);
+  {
+    std::string str{"Dynamic String"};
+    stack.push(str);
+  }
 
   print(stack);
+  assert(not stack.empty());
+  assert(stack.size() == 8);
+  assert(stack[1] == luxx::nil);
+  assert(stack[2] == -123);
+  assert(stack[3] == 3.14);
+  assert(stack[4] == 1.0);
+  assert(stack[5] == "a string");
+  assert(stack[6] == true);
+  assert(stack[7] == false);
+  assert(stack[8] == "Dynamic String");
 
-  // stack[1] = "another string";
-  // stack[2] = 456;
-  // stack[5] = "actually a new value...";
+  stack.rotate(6, -1);
+  stack[1] = "another string";
+  stack[2] = 456;
+  stack[4] = stack[3];
+  stack[5] = "123456789";
+  stack[6] = static_cast<luxx::integer>(stack[5]);
+  stack.push(stack[2]);
 
-  // const auto x   = static_cast<int>(stack[2]);
-  // const auto str = static_cast<std::string_view>(stack[1]);
+  print(stack);
+  assert(not stack.empty());
+  assert(stack.size() == 9);
+  assert(stack[1] == "another string");
+  assert(stack[2] == 456);
+  assert(stack[3] == 3.14);
+  assert(stack[4] == 3.14);
+  assert(stack[5] == "123456789");
+  assert(stack[6] == 123456789);
+  assert(stack[7] == "Dynamic String");
+  assert(stack[8] == true);
+  assert(stack[9] == 456);
 }
